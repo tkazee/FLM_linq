bdda1d56374c4cbdf49e54956ac8b8acdbe240c7
Azeem <Azeemtk9@gmail.com>
2026-03-05 17:12:15 +0530

    Refactor Product and Student classes; clean up Program.cs
    Refactored the `Product` and `Student` classes:
    - Made both classes `public` for broader accessibility.
    - Added properties and constructors to encapsulate data.
    - Implemented `ToString` methods for better object representation.
    - Introduced a static field `_pcodeCounter` in `Product` for unique product codes.
    
    Cleaned up `Program.cs` by removing unused code.

--- Product.cs
+++ Product.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace FLM_linq
 {
-    internal class Product
+    public class Product
     {
+        private static int _pcodeCounter = 1001;
+        public int Pcode { get; set; }
+        public string Pname { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public Product(string pname, int price, int quantity)
+        {
+            Pcode=_pcodeCounter++;
+            Pname = pname;
+            Price = price;
+            Quantity = quantity;
+        }
+        public override string ToString()
+        {
+            return "Product Id : "+ Pcode+", Product Name: "+ Pname+", Product cost: "+ Price + ", Available Quntity: "+Quantity;
+        }
     }
 }
--- Student.cs
+++ Student.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace FLM_linq
 {
-    internal class Student
+    public class Student
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+        public int Marks { get; set; }
+
+        public Student(int id, string name, string course, int marks)
+        {
+            Id = id;
+            Name = name;
+            Course = course;
+            Marks = marks;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Course: {Course}, Marks: {Marks}";
+        }
+
     }
 }
