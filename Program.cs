c52602cc36f0b70193110ce54aa3db07eee755b2
Azeem <Azeemtk9@gmail.com>
2026-03-05 17:04:58 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FLM_linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
